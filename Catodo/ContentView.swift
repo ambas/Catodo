89b314bab7913341e7f5ba62e333af848e6bcfad
Ambas <ambaschobsanti@gmail.com>
2023-11-16 14:20:19 +0700

    Implemente Create / Delete / Toggle complete features

--- Catodo/ContentView.swift
+++ Catodo/ContentView.swift
@@ -7,28 +7,73 @@
 
 import SwiftUI
 
+
+typealias TaskID = String
+
+@Observable final class Store {
+    func addTask(_ item: Item) {
+        items.append(item)
+    }
+    
+    func delete(_ id: TaskID) {
+        items.removeAll { $0.id == id }
+    }
+    
+    func toggleComplete(_ id: TaskID) {
+        let index = items.firstIndex { item in
+            item.id == id
+        }!
+        items[index].isComplete.toggle()
+    }
+    
+    private(set) var items: [Item] = Item.mock
+    
+}
+
 struct Item: Identifiable {
     let taskName: String
-    let isComplete: Bool
-    var id: String { taskName }
+    var isComplete: Bool
+    var id: String { taskName } // TODO: Make it to be proper id
     init(taskName: String, isComplete: Bool = false) {
         self.taskName = taskName
         self.isComplete = isComplete
     }
+    
+    static let mock = (0..<1).map { Item(taskName: "item : \($0)") }
 }
-let mock = (0..<10).map { Item(taskName: "item : \($0)") }
 struct ContentView: View {
-    @State var items: [Item] = mock
+    
+    let store: Store = Store()
+    @State private var newTaskTitle = ""
+    @State private var showAddNewTaskAlert = false
+    
     var body: some View {
-        LazyVStack {
+        
+        NavigationStack {
             ScrollView {
-                ForEach(items) { item in
-                    ItemView(item)
-                        .onTapGesture {
-                            print("aa")
-                        }
-                    Divider()
+                LazyVStack {
+                    ForEach(store.items) { item in
+                        ItemView(item: item, onDelete: { store.delete($0) })
+                            .onTapGesture {
+                                store.toggleComplete(item.id)
+                            }
+                        Divider()
+                    }
                 }
+            }
+            .toolbar {
+                ToolbarItemGroup {
+                    Button(action: {
+                        showAddNewTaskAlert = true
+                    }, label: {
+                        Image(systemName: "plus.app")
+                    })
+                }
+            }
+            .alert("Add New Task", isPresented: $showAddNewTaskAlert) {
+                TextField("new task", text: $newTaskTitle)
+                Button("Add", action: { store.addTask(.init(taskName: newTaskTitle)); newTaskTitle = "" })
+                Button("Cancle", action: { })
             }
         }
     }
@@ -36,11 +81,9 @@ struct ContentView: View {
 
 struct ItemView: View {
     let item: Item
+    let onDelete: (TaskID) -> Void
     let fontSize: CGFloat = 30
     
-    init(_ item: Item) {
-        self.item = item
-    }
     var body: some View {
         VStack {
             HStack {
@@ -52,6 +95,9 @@ struct ItemView: View {
                     icon: { Image(systemName: item.isComplete ? "circle.fill" : "circle") }
                 )
                 .font(.system(size: fontSize))
+                Button(action: { onDelete(item.id) }, label: {
+                    Image(systemName: "bin.xmark.fill")
+                })
             }
             .padding()
             .contentShape(Rectangle())
@@ -61,13 +107,11 @@ struct ItemView: View {
 
 #Preview {
     ContentView()
-        .frame(width: 500, height: 500)
 }
 
 #Preview("Item View") {
     Group {
-        ItemView(.init(taskName: "sample1"))
-        ItemView(.init(taskName: "sample2", isComplete: true))
+        ItemView(item: .init(taskName: "sample1"), onDelete: { _ in })
     }
 }
 
